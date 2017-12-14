e03bc68c18e68e8d6b2de87330e5fa10922bc34b
Rod McBride <rmcbride@toplinestrategies.com>
2017-12-13 18:18:24 -0700

    Completed Main Layout

--- App.js
+++ App.js
@@ -5,8 +5,21 @@ export default class App extends React.Component {
     render() {
         return (            
             <View style={styles.container}>
+
                 <View style={styles.header}>
                     <Text style={styles.headerText}>- NOTER -</Text>
+                </View>
+
+                <ScrollView style={styles.scrollContainer}></ScrollView>
+                
+                <View style={styles.footer}>
+                    <TouchableOpacity style={styles.addButton}>
+                        <Text style={styles.addButtonText}>+</Text>
+                    </TouchableOpacity>
+
+                    <TextInput style={styles.textInput} 
+                        placeholder='> note' placeholderTextColor='white' underlineColorAndroid='transparent'>
+                    </TextInput>
                 </View>
             </View>
         );
@@ -29,4 +42,40 @@ const styles = StyleSheet.create({
         fontSize: 18,
         padding: 26,
     },
+    scrollContainer: {
+        flex: 1,
+        marginBottom: 100,
+    },
+    footer: {
+        position: 'absolute',
+        alignItems: 'center',
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+    addButton: {
+        backgroundColor: '#E91E63',
+        width: 90,
+        height: 90,
+        borderRadius: 50,
+        borderColor: '#ccc',
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 8,
+        marginBottom: -45,
+        zIndex: 10,
+    },
+    addButtonText: {
+        color: '#fff',
+        fontSize: 24,
+    },
+    textInput: {
+        alignSelf: 'stretch',
+        color: '#fff',
+        padding: 20,
+        paddingTop: 46,
+        backgroundColor: '#252525',
+        borderTopWidth: 2,
+        borderTopColor: '#ededed',
+    },    
 });
